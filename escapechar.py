fc4ad58b9bcb7657abdc1536f53c48cf351dd511
Harsha Pocharam <harshanaayini@gmail.com>
2020-04-04 20:30:08 -0400

    escape split and special charectors in python

---
+++ escapechar.py
@@ -0,0 +1,18 @@
+splitString="This string has been\n split over\n several \nlines"
+print(splitString)
+
+tabbedString="1\t2\t3\3\t4"
+print(tabbedString)
+
+print('The pet shops owner said "No, no\'e\'s uh,---".')
+
+anotherSplitString = """This string has been
+splited over
+several lines"""
+print(anotherSplitString)
+
+escapeSplitString = """This string has been \
+splited over \
+several lines"""
+print(escapeSplitString)
+
