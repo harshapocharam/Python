cacb48f8f39aa2db909b65673bec2af41aeee5b5
Harsha Pocharam <harshanaayini@gmail.com>
2020-04-04 20:17:30 -0400

    print and strings in python

---
+++ strings.py
@@ -0,0 +1,10 @@
+print("Today is the good day to learn Python")
+print('Python is fun')
+print("Python is string are easy to use")
+print('we can even include "quotes" in strings')
+print("hello" +  "world")
+
+greeting =input("please enter your quotes:") 
+name ="brues"
+
+print(greeting + ' ' + name)
