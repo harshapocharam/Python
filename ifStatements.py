7c694a384fd9c2d6b9bfd8c0f1ff66c90f138000
Harsha Pocharam <harshanaayini@gmail.com>
2020-05-17 11:40:25 -0400

    string and flow controls

---
+++ ifStatements.py
@@ -0,0 +1,11 @@
+name = input("Please enter your name: ")
+age = int(input("How old are you, {0}? ".format(name)))
+print(age)
+
+if age >= 18:
+    print("you are old enough to vote")
+    print("please put on X in block")
+else:
+    print("please come back in {0} years" .format(18-age))
+# elif age == 900:
+#     print("invalid")
