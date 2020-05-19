d9f35fe8eb2b77f189df55b11805dd8cb66093e3
Harsha Pocharam <harshanaayini@gmail.com>
2020-05-19 10:16:24 -0400

    if else condition for age in python loops

--- ifStatements.py
+++ ifStatements.py
@@ -8,3 +8,11 @@ if age >= 18:
 else:
     print("please come back in {0} years" .format(18-age))
 
+ages =int(input("please enter your age : "))
+
+if ages >=16 and ages <= 65:
+    print("age bitween 16 and 65")
+elif ages < 16:
+    print("age is greater then 16 ")
+elif ages > 65:
+    print("age is less then 65")
